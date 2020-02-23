2f11f4f0c5a3032ccfd69ff27388c72f555368a4
Fernando dos Santos <nando4803@gmail.com>
2020-02-23 19:30:20 +0100

    Refatorando js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-$(document).ready(function () {
-  var showHeaderAt = 150;
-
-  var win = $(window),
-    body = $('body');
-
-  if (win.width() > 400) {
-    win.on('scroll', function (e) {
-      if (win.scrollTop() > showHeaderAt) {
-        body.addClass('fixed');
-      } else {
-        body.removeClass('fixed');
-      }
-    });
-  }
-});
 const container = document.querySelector(".container");
 const companies = [{
     name: "BukaApp",
